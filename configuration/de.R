9ce75ff4da11cac9b1b623c9026efb48da8ed73f
suhita ghosh <sughosh@isgintern>
2019-01-08 19:47:15 +0100

    added exception handling in clasification code
    process notebook changes

--- configuration/de.R
+++ configuration/de.R
@@ -1,4 +1,4 @@
-doc_number <- 200
+doc_number <- 80
 
 lang <- "german"
 fileName <- "../data/german/acquis_german.cf"
@@ -7,4 +7,4 @@ incFileName <- "../output/inc_DE"
 labelFile <- "../data/german/desc_de.xml"
 model_file <- "../output/german-gsd-ud-2.3-181115.udpipe"
 
-batch_number <- 5
+batch_number <- 4
--- scripts/classification_utility.R
+++ scripts/classification_utility.R
@@ -1,10 +1,13 @@
 train_ratio <- 0.65
 test_ratio <- 0.35
 
-classify <- function(mldrfilename, mode, classifier, reduceLabels) {
+classify <- function(mldrfilename,
+                     mode,
+                     classifier,
+                     reduceLabels) {
   for (index in 1:batch_number) {
     ds <- mldr(paste(mldrfilename, index, sep = ""))
-    if(reduceLabels == TRUE)
+    if (reduceLabels == TRUE)
       ds <- ds[.SCUMBLE <= ds$measures$scumble]
     ds <- remove_skewness_labels(ds, 1) %>%
       remove_attributes("...") %>%
@@ -13,29 +16,48 @@ classify <- function(mldrfilename, mode, classifier, reduceLabels) {
       create_holdout_partition(c(train = train_ratio, test = test_ratio))
     
     if (tolower(mode) == "lp")
-      model <- lp(ds$train, classifier)
+      cl_model <- lp(ds$train, classifier)
     else if (tolower(mode) == "cc")
-      model <- cc(ds$train, classifier)
+      cl_model <- cc(ds$train, classifier)
     else
-      model <- br(ds$train, classifier)
+      cl_model <- br(ds$train, classifier)
     
-    prediction <- predict(model, ds$test)
-    temp <- multilabel_evaluate(ds$test, prediction, "bipartition")
+    prediction <- tryCatch(
+      predict(cl_model, ds$test),
+      error = function(e) {
+        message(paste("prediction could not be done for ", paste(mldrfilename, index, sep = "")))
+        NA
+      }
+    )
     
-    rm(model)
+    temp <-
+      tryCatch(
+        multilabel_evaluate(ds$test, prediction, "bipartition"),
+        error = function(e) {
+          message(paste("evaluation could not be done for ", paste(mldrfilename, index, sep = "")))
+          NA
+        }
+      )
+    
+    if (index == 1 && !is.na(temp)) {
+      if (exists("cls_pred"))
+        rm(cls_pred)
+      cls_pred <- temp
+      print("Classification done for batch 1")
+      
+    } else if (index > 1 && !is.na(temp)) {
+      if(!exists('cls_pred'))
+        cls_pred <- temp
+      else
+      cls_pred <- cbind(cls_pred, temp)
+      print(paste("Classification done for batch", index))
+    }
+    rm(cl_model)
+    rm(ds)
     rm(prediction)
     
-    if (index == 1) {
-      if(exists("cls_pred")) rm(cls_pred)
-      cls_pred <- temp
-      
-    } else{
-      cls_pred <- cbind(cls_pred, temp)
-    }
-    print(paste("Classification done for batch", index))
   }
   
-  rm(temp)
   prediction_mean <- rowMeans(cls_pred, na.rm = TRUE)
   
   prediction_mean
