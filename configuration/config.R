4e9c85000446e3635df4fb2630a1f88f89ffd3b3
suhitaghosh10 <suhita.ghosh.10@gmail.com>
2019-01-15 01:58:42 +0100

    moved markdown files to docs

--- configuration/config.R
+++ configuration/config.R
@@ -1,4 +1,5 @@
+## The number of documents on which exploratory analysis and classification will be carried out
 doc_number <- 100
+## The number of batches of datasets to be created for classification
+## batch_size <- doc_number/batch_number
 batch_number <- 20
-
-#doc_number <-  batch_size * batch_number
