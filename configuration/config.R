9c38b50d27b568e50e5efd953757aeea6568cf58
suhita <suhitaghosh10@users.noreply.github.com>
2019-01-16 17:15:03 +0100

    Update config.R

--- configuration/config.R
+++ configuration/config.R
@@ -1,5 +1,6 @@
 ## The number of documents on which exploratory analysis and classification will be carried out
 doc_number <- 100
+
 ## The number of batches of datasets to be created for classification
 ## batch_size <- doc_number/batch_number
 batch_number <- 5
